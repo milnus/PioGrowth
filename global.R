8bbfac5bd8bf06260325a8cf6967889eb3204b95
Magnus G. Jespersen <44769523+milnus@users.noreply.github.com>
2025-01-23 13:32:27 +0100

    Update global.R
    Add in fix for broken download button from Chromium browsers

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
-# Load all libraries
+## Load all libraries
 library(shiny)
 library(bslib)
 library(stringr)
@@ -7,11 +7,19 @@ library(gridExtra)
 library(devtools)
 devtools::load_all("R")
 
-# Source all modules
+## Source all modules
 source("modules/read_data_module.R")
 source("modules/plot_raw_module.R")
 source("modules/filter_reactor_module.R")
 source("modules/calibration_module.R")
 
-# Set options
+## Set options
 options(shiny.maxRequestSize = 100 * 1024^2) # Increase file size for uploads
+
+## Global functions
+# Workaround for Chromium Issue 468227
+downloadButton <- function(...) {
+  tag <- shiny::downloadButton(...)
+  tag$attribs$download <- NULL
+  tag
+}
