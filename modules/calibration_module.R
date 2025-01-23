11e868ba810b7fbc495382d146840d0e93fe4a4e
Magnus G. Jespersen <44769523+milnus@users.noreply.github.com>
2025-01-23 13:36:38 +0100

    Update calibration_module.R
    Add in ui and initial observe for uploading calibration file

--- modules/calibration_module.R
+++ modules/calibration_module.R
@@ -1,11 +1,16 @@
 calibration_ui <- function(id) {
   ns <- NS(id)
   tagList(
+	# Upload of calibration value csv
+	fileInput(ns("upload_calibration_file"), label = "Manual OD measurments table", accept = c(".csv", ".txt")), 
+
+	# Fixed intercept input box
     checkboxInput(
       inputId = ns("fixed_intercept"),
       label = "Force intercept through origin",
       value = FALSE
     ),
+	# Origin point addition option
     uiOutput(ns("zero_point_box"))
   )
 }
@@ -25,6 +30,9 @@ calibration_server <- function(id) {
     })
 
     # More server functions
-
+	observe({
+		req(input$upload_calibration_file)
+		message(paste("Upload calibration file:", input$upload_calibration_file$datapath))
+	})
   })
 }
