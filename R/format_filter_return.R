1b2aea824fc0f002c0673a10d17dc2aea6e93946
Magnus G. Jespersen <44769523+milnus@users.noreply.github.com>
2025-07-08 12:01:47 +0200

    pass file_path from filtering module

--- R/format_filter_return.R
+++ R/format_filter_return.R
@@ -5,8 +5,9 @@ format_filter_return <- function(
   filt_strat
 ) {
   return_list <- list(
-    "raw_data" = raw_data,
-    "filtered_data" = filtered_data,
+    "raw_data" = raw_data[["pioreactor_OD_data_wide"]],
+    "filtered_data" = filtered_data[["pioreactor_OD_data_wide"]],
+    "file_path" = raw_data[["file_path"]],
     "filtering_state" = list(
       "reactors_selected" = reactor_selection,
       "filtering_strategy" = filt_strat
